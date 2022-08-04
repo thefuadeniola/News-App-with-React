7c7872f6e94fa90932fd7d359b34d6a3b4d02981
Olaleye Hairat <SammieKei40>
2022-08-04 03:41:09 +0100

    Links Updated

--- src/headline/headline.js
+++ src/headline/headline.js
@@ -7,14 +7,14 @@ export default function Headline(props) {
     const headlinerthree = props.news[2]
     return(
         <div className='headlines'>
-            <Link to={{pathname: headliner.url}} style={{textDecoration:'none'}} target=''>
+            <Link to={{pathname: headliner.url}} style={{textDecoration:'none'}} target='_blank' rel='noreferrer'>
                 <div className='main__headline' style={{backgroundImage:`url(${headliner.urlToImage})`}}>
                     <div className='headline__overlay'><h2 className='headline__text'>{headliner.title}</h2></div>   
                 </div>
             </Link>
             <div className='sub-headlines'>
             {props.news.length > 1 && 
-            <Link to={{pathname: headlinertwo.url}} style={{textDecoration:'none'}} target=''>
+            <Link to={{pathname: headlinertwo.url}} style={{textDecoration:'none'}} target='_blank' rel='noreferrer'>
             <div style={{backgroundImage:`url(${headlinertwo.urlToImage})`}} className='headline__two'>
                 <div className='headline__overlay'><h3>{headlinertwo.title}</h3></div>   
             </div>
@@ -22,7 +22,7 @@ export default function Headline(props) {
     
             }
             {props.news.length >2 &&
-            <Link to={{pathname: headlinerthree.url}} style={{textDecoration:'none'}} target=''>
+            <Link to={{pathname: headlinerthree.url}} style={{textDecoration:'none'}} target='_blank' rel='noreferrer'>
             <div style={{backgroundImage:`url(${headlinerthree.urlToImage})`}} className='headline__three'>
                 <div className='headline__overlay'><h3>{headlinerthree.title}</h3></div>   
             </div>
--- src/main/main.js
+++ src/main/main.js
@@ -6,7 +6,7 @@ export default function Main (props) {
     const mainNewsItems = props.news
     const newsList = mainNewsItems.map((mainNewsItem) => {
         return (
-            <Link to={{pathname: mainNewsItem.url}}  style={{textDecoration:'none'}} target='_blank'>
+            <Link to={{pathname: mainNewsItem.url}}  style={{textDecoration:'none'}} target='_blank' rel='noreferrer'>
                 <div className='news__box'>
                     <div className='news__box-image' style={{backgroundImage:`url(${mainNewsItem.urlToImage})`, 
                 backgroundSize: 'contain', backgroundRepeat:'no-repeat'}}></div>
